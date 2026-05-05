100f992ad4b411e5902b30b6e36e12662ca8167f
Arpit ghodke <arpitghodke07@gmail.com>
2026-05-05 12:25:23 +0530

    Rename next.config.ts to mjs for GitHub Pages actions compatibility

--- next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  output: "export",
-  // Optional: Set basePath if hosting on a sub-path, e.g., https://username.github.io/repo-name/
-  // basePath: "/trans",
-};
-
-export default nextConfig;
